c4eb70cf8626b272a92fefa2b57408027a52130e
Muhammad Daivasmara Denaw <daivasmara.denaw@rebelworks.co>
2018-08-27 16:27:07 +0700

    fixed calculation proccess

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,19 +4,24 @@ new Vue({
     display: '0',
     operators: '',
     number1: '0',
-    number2: '0'
+    number2: '0',
+    calculation: false
   },
   methods: {
     append: function(number){
       if (this.display === '0' || this.display === 'Infinity' || this.display === 'NaN'){
         this.display = '',
         this.display += number
+      } else if (this.calculation === true) {
+        this.display = '',
+        this.display += number,
+        this.calculation = false
       } else {
         this.display += number
       }
     },
     dot: function(){
-      if (this.display === '0') {
+      if (this.display === '0' || this.calculation === true) {
         this.append('0.')
       } else if (this.display.indexOf('.') === -1) {
         this.append('.')
@@ -25,8 +30,9 @@ new Vue({
     ac: function(){
       this.display = '0',
       this.operators = '',
-      this.number1 = '',
-      this.number2 = ''
+      this.number1 = '0',
+      this.number2 = '0',
+      this.calculation = false
     },
     backspace: function(){
       this.display = this.display.slice(0, -1),
@@ -38,23 +44,24 @@ new Vue({
     },
     division: function(){
       this.number1 = this.display,
-      this.display = '0',
+      this.calculation = true,
       this.operators = '&#247;'
     },
     times: function(){
       this.number1 = this.display,
-      this.display = '0',
+      this.calculation = true,
       this.operators = 'x'
     },
     substraction: function(){
       this.number1 = this.display,
-      this.display = '0',
+      this.calculation = true,
       this.operators = '-'
     },
     plus: function(){
       this.number1 = this.display,
-      this.display = '0',
+      this.calculation = true,
       this.operators = '+'
+
     },
     result: function(){
       this.number2 = this.display,
@@ -63,7 +70,8 @@ new Vue({
       this.operators === '&#247;' ? this.display =  String(parseFloat(this.number1)/parseFloat(this.number2)) :
       this.operators === 'x' ? this.display =  String(parseFloat(this.number1)*parseFloat(this.number2)) :
       this.display,
-      this.operators = ''
+      this.operators = '',
+      this.calculation = false
     }
   }
 });
